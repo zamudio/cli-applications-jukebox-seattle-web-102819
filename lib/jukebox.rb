152339c9ac52c7e6a7d87319ce065e95172c09f7
Michael Z <mziacm@gmail.com>
2019-10-15 10:53:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,10 +12,10 @@ songs = [
 
 def help
   puts "I accept the following commands:"
-  puts "--help: displays this help message"
-  puts "--list: displays a list of songs you can play"
-  puts "--play: lets you choose a song to play"
-  puts "--exit: exits this program"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 def list(songs)
