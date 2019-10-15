571931ff55a219d0af7489b5c586e0e53800db4b
Michael Z <mziacm@gmail.com>
2019-10-15 11:07:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   if songs.include?(user_input)
     puts "Playing #{user_input}"
   elsif (user_input.to_i).between?(1,9)
-    puts "Playing #{user_input}"
+    puts "Playing #{user_input.to_i}"
   else
     puts "Invalid input, please try again"
   end
