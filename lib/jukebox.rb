21f1504af3794e533a22f2036675f42a825d1677
Michael Z <mziacm@gmail.com>
2019-10-15 11:15:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
   if (user_input.to_i).between?(1,9)
-    puts "Playing "
+    puts "Playing #{songs[user_input.to_i - 1]}"
   elsif songs.include?(user_input)
     puts "Playing #{user_input}"
   elsif (user_input.to_i).between?(1,9)
